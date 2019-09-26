4449573ad9a8f94ba1601eefd505004d3ea514fa
danielpeng95 <danielpeng95@yahoo.com>
2019-09-25 18:04:03 -0600

    updata commit

--- tests/yoodlize.js
+++ tests/yoodlize.js
@@ -10,54 +10,54 @@ module.exports = {
     'see all/home/browse': browser => {
         browser
             .useXpath()
-            // .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[1]', 10000) //see all RV
-            // .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[1]') //see all RV
-            // .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 8000)
-            // .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Recreational Vehicles")
-            // .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
-            // .click('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[1]', 10000) //see all RV
+            .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[1]') //see all RV
+            .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 8000)
+            .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Recreational Vehicles")
+            .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .click('//img[@src="/images/logo/blueRaw.png"]') //home img
 
-            // .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[2]', 10000) //see all
-            // .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[2]') //see all
-            // .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 8000)
-            // .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Outdoor Gear")
-            // .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
-            // .click('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[2]', 10000) //see all
+            .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[2]') //see all
+            .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 8000)
+            .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Outdoor Gear")
+            .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .click('//img[@src="/images/logo/blueRaw.png"]') //home img
 
-            // .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[3]', 10000) //see all
-            // .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[3]') //see all
-            // .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 8000)
-            // .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Electronics")
-            // .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
-            // .click('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[3]', 10000) //see all
+            .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[3]') //see all
+            .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 8000)
+            .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Electronics")
+            .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .click('//img[@src="/images/logo/blueRaw.png"]') //home img
 
-            // .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[4]', 10000) //see all
-            // .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[4]') //see all
-            // .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 10000)
-            // .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Party")
-            // .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
-            // .click('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[4]', 10000) //see all
+            .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[4]') //see all
+            .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 10000)
+            .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Party")
+            .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .click('//img[@src="/images/logo/blueRaw.png"]') //home img
 
-            // .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[5]', 10000) //see all
-            // .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[5]') //see all
-            // .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 10000)
-            // .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Tools")
-            // .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
-            // .click('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[5]', 10000) //see all
+            .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[5]') //see all
+            .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 10000)
+            .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Tools")
+            .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .click('//img[@src="/images/logo/blueRaw.png"]') //home img
 
-            // .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[6]', 10000) //see all
-            // .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[6]') //see all
-            // .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 10000)
-            // .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Clothing")
-            // .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
-            // .click('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[6]', 10000) //see all
+            .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[6]') //see all
+            .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 10000)
+            .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Clothing")
+            .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .click('//img[@src="/images/logo/blueRaw.png"]') //home img
 
-            // .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[7]', 10000) //see all
-            // .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[7]') //see all
-            // .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 10000)
-            // .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Home")
-            // .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
-            // .click('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .waitForElementPresent('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[7]', 10000) //see all
+            .click('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[7]') //see all
+            .waitForElementPresent('//div[@class="sc-jKVCRD jSqgxr"]', 10000)
+            .verify.containsText('//div[@class="sc-jKVCRD jSqgxr"]', "Home")
+            .waitForElementPresent('//img[@src="/images/logo/blueRaw.png"]') //home img
+            .click('//img[@src="/images/logo/blueRaw.png"]') //home img
 
             .waitForElementPresent('(//a[@class="sc-esjQYD cZSeUZ"])[16]', 10000) //Browse Categories
             .click('(//a[@class="sc-esjQYD cZSeUZ"])[16]') //Browse Categories
@@ -102,9 +102,7 @@ module.exports = {
             .verify.visible('(//div[@emphasis="true"])[1]')
 
             .pause(3000)
-
-            
-
+        
     }
 }
 
