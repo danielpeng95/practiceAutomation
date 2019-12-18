d50adf057c30287a9c57b464b6db58fefb1136b6
danielpeng95 <danielpeng95@yahoo.com>
2019-12-18 12:17:20 -0500

    switch to 1st test

--- pageObjects/typeTestPageObject.js
+++ pageObjects/typeTestPageObject.js
@@ -3,15 +3,15 @@ module.exports = {
      *    this is my first type Test
      *      this is a basic test
      * */
-    // url: 'https://www.keyhero.com/free-typing-test/',
+    url: 'https://www.keyhero.com/free-typing-test/',
 
-    // elements: {
-    //     start: '[data-reactid=".0.0.1.1.2"]',
-    //     current: '.quote',
-    //     input: '[class="user-input-text"]',
-    //     WPM: '[data-reactid=".0.1.1.1.0"]',
-    //     Accuracy: '[data-reactid=".0.1.1.3.0"]'
-    // }
+    elements: {
+        start: '[data-reactid=".0.0.1.1.2"]',
+        current: '.quote',
+        input: '[class="user-input-text"]',
+        WPM: '[data-reactid=".0.1.1.1.0"]',
+        Accuracy: '[data-reactid=".0.1.1.3.0"]'
+    }
 
 
     /**
@@ -20,15 +20,15 @@ module.exports = {
         this test is kinda buggy, it works most of the time tho.
         but once in a while, it will not work.
     **/
-    url: 'https://www.ratatype.com/typing-test/test/',
+    // url: 'https://www.ratatype.com/typing-test/test/',
 
-    elements: {
-        start: '[class="submit gr"]',
-        current: '[class="mainTxt"]',
-        input: '[spellcheck="false"]',
-        WPM: { selector: '(//div[@class="blue"])[1]', locateStrategy: 'xpath' },
-        Accuracy: { selector: '(//div[@class="blue"])[2]', locateStrategy: 'xpath' }
-    }
+    // elements: {
+    //     start: '[class="submit gr"]',
+    //     current: '[class="mainTxt"]',
+    //     input: '[spellcheck="false"]',
+    //     WPM: { selector: '(//div[@class="blue"])[1]', locateStrategy: 'xpath' },
+    //     Accuracy: { selector: '(//div[@class="blue"])[2]', locateStrategy: 'xpath' }
+    // }
 
     
 }
